eb4ecc877b06428839111f686a1cf19f8237341a
Anouluck MANY <bigziphone@gmail.com>
2023-09-18 18:01:29 +0700

    update

---
+++ Card-Matching/Pages/Index.razor.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Components;
+using Share.Model.Card;
+
+namespace CardMatching.Pages
+{
+    public partial class Index
+    {
+        static string[] CardFace = { "🔥", "🗿", "👁️", "💀", "😭", "🤨"};
+
+        Deck _deck = new Deck(cardFace: CardFace);
+
+        bool _isChecking = false;
+
+        protected override void OnInitialized()
+        {
+            _deck.CreateDeck();
+        }
+
+        public async Task OnOpenCard(string? id)
+        {
+            if (_isChecking) return;
+
+            var isSuccess = _deck.ChangeCardStatus(id, CardStatus.Open);
+
+            if (isSuccess) 
+            { 
+                _isChecking = true;
+
+                await CheckForMatchingItems();
+
+                _isChecking = false;
+            }
+        }
+
+        public async Task CheckForMatchingItems()
+        {
+            var result = _deck.CheckForMatchingCards();
+
+            if (!result.isChecked) return;
+
+            if (!result.isMatched)
+            {
+                await Task.Delay(1000);
+            }
+
+            foreach (var card in result.openedCards)
+            {
+                var status = result.isMatched ? CardStatus.Matched : CardStatus.Close;
+
+                _deck.ChangeCardStatus(card.Id, status);
+            }
+        }
+
+        public void Refresh()
+        {
+            _deck.CreateDeck();
+        }
+    }
+
+    public enum GameStatus
+    {
+        Started,
+        Paused,
+        Finished
+    }
+}
---
+++ Share/Model/Card/Deck.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Share.Model.Card
+{
+    public class Deck
+    {
+        public List<CardModel> Cards { get; set; }
+        private string[] CardFaces { get; set; }
+        public Deck(string[] cardFace) 
+        {
+            Cards = new List<CardModel>();
+            CardFaces = cardFace;
+        }
+
+        public void CreateDeck()
+        {
+            Cards.Clear();
+
+            Random _randomGenerator = new Random();
+
+            foreach (var card in CardFaces)
+            {
+                for (var i = 0; i < 2; i++)
+                {
+                    var id = Guid.NewGuid().ToString();
+                    Cards.Add(new CardModel
+                    {
+                        Id = id,
+                        Name = card
+                    });
+                }
+            }
+
+            //random items inside the list
+            Cards.Sort((x, y) => _randomGenerator.Next(-1, 2));
+        }
+        public bool ChangeCardStatus(string? id, CardStatus status)
+        {
+            var card = Cards.FirstOrDefault(x => x.Id == id);
+
+            if (card == null) return false;
+
+            if (card.Status == status) return false;
+
+            card.Status = status;
+
+            return true;
+        }
+
+        public List<CardModel> GetOpenedCards() 
+        {
+            return Cards.Where(card => card.Status == CardStatus.Open).ToList();
+        }
+
+        public (bool isChecked, bool isMatched, List<CardModel> openedCards) CheckForMatchingCards()
+        {
+            var openedCards = GetOpenedCards();
+
+            if (openedCards.Count < 2) return (false, false, openedCards);
+
+            var isMatched = openedCards[0].Name == openedCards[1].Name;
+
+            return (true, isMatched, openedCards);
+        }
+    }
+}
