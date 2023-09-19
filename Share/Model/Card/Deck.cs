ad8a022a1828a1359e86dea6d610251505293a50
Anouluck MANY <bigziphone@gmail.com>
2023-09-19 15:39:29 +0700

    change project name

--- Share/Model/Card/Deck.cs
+++ Share/Model/Card/Deck.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using CardMatching.Pages;
-
-namespace Share.Model.Card
+﻿namespace Share.Model.Card
 {
     public class Deck
     {
